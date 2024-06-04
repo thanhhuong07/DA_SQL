254c65328f6d222a85af305a00354f06da7c82ac
Huong Nguyen Vo Thanh <107316840+thanhhuong07@users.noreply.github.com>
2024-06-04 22:38:26 +0700

    Update practice_4.sql

--- practice_4.sql
+++ practice_4.sql
@@ -9,6 +9,28 @@ END) AS mobile_views
 FROM viewership
 
 --ex2
+SELECT x,y,z,
+CASE WHEN (x+y)>z AND (x+z)>y AND (y+z)>x THEN 'Yes' ELSE 'No' END AS triangle
+FROM Triangle
+  
+--ex3
+--bài này em không run được, em note lại cách làm của em ạ
+SELECT 
+ROUND(100*COUNT(CASE WHEN call_category='n/a' OR call_category= NULL THEN 1 ELSE 0 END)/COUNT(case_id),1) AS uncategorised_call_pct
+FROM callers
+
+--ex4
+SELECT name FROM Customer
+WHERE NOT referee_id=2 OR referee_id is NULL
+
+-ex5
+SELECT survived,
+SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY survived
+
 
 
 

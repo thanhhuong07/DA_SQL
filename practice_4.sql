051332fbc25b368a07c609fb9f4706b6c1a294ed
Huong Nguyen Vo Thanh <107316840+thanhhuong07@users.noreply.github.com>
2024-06-04 21:58:00 +0700

    Create practice_4.sql

---
+++ practice_4.sql
@@ -0,0 +1,21 @@
+--ex1
+SELECT 
+SUM(CASE 
+WHEN device_type='laptop' THEN 1 ELSE 0
+END) AS laptop_views,
+SUM(CASE 
+WHEN device_type IN ('tablet','phone')THEN 1 ELSE 0
+END) AS mobile_views
+FROM viewership
+
+--ex2
+
+
+
+
+
+
+
+
+
+
